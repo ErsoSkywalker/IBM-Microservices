023180fe4e55562758b6f82fc990020f6debb875
Christian Lynch Ontiveros <contiverosv@outlook.com>
2021-09-07 18:51:30 -0500

    Tarea Herencia

--- TiposHerencia/src/com/ChristianOntiveros/TiposHerencia/Main.java
+++ TiposHerencia/src/com/ChristianOntiveros/TiposHerencia/Main.java
@@ -11,9 +11,9 @@ public class Main {
     public static void main(String[] args) {
 
         Cotorro chocotorro = new Cotorro();
-        
+        chocotorro.volar();
 
-        Animal gato = new Gato();
+        Ente gato = new Gato();
         gato.existir();
 
     }
